8eb332cba9619fa8ffd320ceb92550f1fdb1fa0e
jamshid <jamshidqochqorov84@gmail.com>
2023-03-03 19:08:26 +0500

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function private(){
+        if(Gate::allows('admin-only',auth()->user()))
+             return view('private');
+        return abort('404','you have not Admin role');
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+# Salom mishqi
+
+Admindan hat senga!
+
+@component('mail::button', ['url' => 'http://127.0.0.1:8000'])
+Visit maening saytimga
+@endcomponent
+
+
+Rahmat
+javobin uchun!
+@endcomponent
